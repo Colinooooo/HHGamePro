b11cccefca4a28be05c926bd0ad05f21775d986f
colin <798361863@qq.com>
2017-10-17 19:58:59 +0800

    Initial Commit

---
+++ HHGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHGame
+//
+//  Created by Colin on 2017/10/17.
+//  Copyright © 2017年 辉宏工作室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
